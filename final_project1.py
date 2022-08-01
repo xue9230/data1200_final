8655f4f28c8ff908408603079f20956de248052b
xue9230 <110322204+xue9230@users.noreply.github.com>
2022-07-31 19:09:01 -0700

    Update final_project1.py

--- final_project1.py
+++ final_project1.py
@@ -2,10 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data=pd.read_csv('survey.csv')
-
+print(data.head())
 
 data.loc[data["mental_health_consequence"]=='Yes',"mental_cons"]=1
 data.loc[data["mental_health_consequence"]=='No',"mental_cons"]=0
 data.loc[data["mental_health_consequence"]=='Maybe',"mental_cons"]=0
 freq=data.groupby('Country')["mental_cons"].sum().reset_index()
 freq.sort_values("mental_cons", ascending=False, inplace=True)
+print(freq.head())
